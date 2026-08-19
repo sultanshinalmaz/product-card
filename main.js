02247c0143e36a4ba4c78cf9cfd12dd8bd93e695
sultanshinalmaz <asultanshin@gmail.com>
2026-08-19 18:16:40 +0500

    Добавил файл homework-10

--- main.js
+++ main.js
@@ -1,63 +1,63 @@
-// Покраска всех карточек
+// // Покраска всех карточек
 
-const productCards = document.querySelectorAll('.product-card');
-const changeColorAllCardButton = document.querySelector('#change-color-all-card');
-const greenColorHash = '#00ff00';
-const blueColorHash = '#0000ff';
-const redColorHash = '#ff0000';
-const yellowColorHash = '#1eff00';
+// const productCards = document.querySelectorAll('.product-card');
+// const changeColorAllCardButton = document.querySelector('#change-color-all-card');
+// const greenColorHash = '#00ff00';
+// const blueColorHash = '#0000ff';
+// const redColorHash = '#ff0000';
+// const yellowColorHash = '#1eff00';
 
-changeColorAllCardButton.addEventListener('click', () => {
-  productCards.forEach((card) => card.style.backgroundColor = greenColorHash)
-})
+// changeColorAllCardButton.addEventListener('click', () => {
+//   productCards.forEach((card) => card.style.backgroundColor = greenColorHash)
+// })
 
-// Покраска одной карточки
+// // Покраска одной карточки
 
-const firstProductCard = document.querySelector('.product-card');
-const changeColorFirstCardButton = document.querySelector('#change-color-first-card');
+// const firstProductCard = document.querySelector('.product-card');
+// const changeColorFirstCardButton = document.querySelector('#change-color-first-card');
 
-changeColorFirstCardButton.addEventListener('click', () => {
-  firstProductCard.style.backgroundColor = blueColorHash;
-})
+// changeColorFirstCardButton.addEventListener('click', () => {
+//   firstProductCard.style.backgroundColor = blueColorHash;
+// })
 
-// Вывод консоль лог
+// // Вывод консоль лог
 
-const outputLogButton = document.querySelector('#output-console-log');
-outputLogButton.addEventListener('click', () => outputConsoleLog('ДЗ №6'))
+// const outputLogButton = document.querySelector('#output-console-log');
+// outputLogButton.addEventListener('click', () => outputConsoleLog('ДЗ №6'))
 
-function outputConsoleLog(message) {
-  console.log(message);
-  alert(message);
-}
+// function outputConsoleLog(message) {
+//   console.log(message);
+//   alert(message);
+// }
 
-//открыть Google
+// //открыть Google
 
-const openGoogleButton = document.querySelector('#open-google');
-openGoogleButton.addEventListener('click', openGoogle)
+// const openGoogleButton = document.querySelector('#open-google');
+// openGoogleButton.addEventListener('click', openGoogle)
 
-function openGoogle() {
-  const answer = confirm('Вы действительно хотите открыть Google?');
+// function openGoogle() {
+//   const answer = confirm('Вы действительно хотите открыть Google?');
 
-  if (answer === true) {
-    window.open('http://google.com')
-  }
-  else {
-    return;
-  }
-}
+//   if (answer === true) {
+//     window.open('http://google.com')
+//   }
+//   else {
+//     return;
+//   }
+// }
 
-// Вывод заголовка в консоль лог при наведении
+// // Вывод заголовка в консоль лог при наведении
 
-const textTitle = document.querySelector('.title');
+// const textTitle = document.querySelector('.title');
 
-textTitle.addEventListener('mouseenter', () => {
-  console.log(textTitle.textContent);
-})
+// textTitle.addEventListener('mouseenter', () => {
+//   console.log(textTitle.textContent);
+// })
 
-// Сменить цвет кнопки при нажатии
+// // Сменить цвет кнопки при нажатии
 
-const changeColorButton = document.querySelector('#change-color-buttom');
+// const changeColorButton = document.querySelector('#change-color-buttom');
 
-  changeColorButton.addEventListener('click', ()=> {
-  changeColorButton.classList.toggle('active');
-})
+//   changeColorButton.addEventListener('click', ()=> {
+//   changeColorButton.classList.toggle('active');
+// })
---
+++ product-cards-file.js
@@ -0,0 +1,46 @@
+export const productCards = [
+  {
+    cardCategory: 'для нормальной кожи',
+    cardName: 'Увлажняющий мусс',
+    cardDescription: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
+    cardCompound: ['активные натуральные комплексы', 'витамины С, А, РР, В и Е', 'солнцезащитные компоненты'],
+    cardPrice: 2750,
+    cardImagine: 'images/bottle_of_mousse.png',
+  },
+
+  {
+    cardCategory: 'для нормальной кожи',
+    cardName: 'Увлажняющая маска',
+    cardDescription: 'Способствует удерживанию влаги в верхних слоях кожи.',
+    cardCompound: ['воски', 'минералы', 'масла'],
+    cardPrice: 3500,
+    cardImagine: 'images/moisturizing_mask.png',
+  },
+
+  {
+    cardCategory: 'для нормальной кожи',
+    cardName: 'Гель для умывания',
+    cardDescription: 'Интенсивно очищает, не повреждает защитный барьер кожи.',
+    cardCompound: ['минералы', 'витамины С, А, РР, В и Е', 'солнцезащитные компоненты'],
+    cardPrice: 1650,
+    cardImagine: 'images/cleansing_gel.png',
+  },
+
+  {
+    cardCategory: 'для нормальной кожи',
+    cardName: 'Подарочный набор №1',
+    cardDescription: 'Набор, состоящий из увлажняющего крема и маски.',
+    cardCompound: ['воски', 'минералы', 'масла'],
+    cardPrice: 4750,
+    cardImagine: 'images/gift_set_№1.png',
+  },
+
+  {
+    cardCategory: 'для нормальной кожи',
+    cardName: 'Подарочный набор №5',
+    cardDescription: 'Весь набор средств Invisible symphony, крем, маска, мусс и гель для умывания.',
+    cardCompound: ['воски', 'минералы', 'масла'],
+    cardPrice: 7520,
+    cardImagine: 'images/gift_set_№5.png',
+  },
+];
